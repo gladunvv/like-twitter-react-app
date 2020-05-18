fad0bfb1cb74db39a5c6643a4c5a24d19f3eeb70
vladislav <bincha.1997@gmail.com>
2020-05-19 00:00:50 +0900

    feat: added components

---
+++ src/components/app/app.js
@@ -0,0 +1,15 @@
+import React from "react";
+import AppHeader from "../app-header";
+import SearchPanel from "../search-panel";
+
+const App = () => {
+  return (
+    <div className="app">
+      <AppHeader />
+      <div className="search-panel d-flex">
+        <SearchPanel />
+      </div>
+    </div>
+  );
+};
+export default App;
